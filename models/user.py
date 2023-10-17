74d3415be8e40da63d52655aef047e1f700184b4
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-10-17 00:33:16 -0700

    update useras a public class attribute

--- models/user.py
+++ models/user.py
@@ -1,23 +1,18 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 """ This module defines all attributes and methods of the User class """
-import models
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ Implementation for User class """
-    bkeys = ["id", "created_at", "updated_at"]
-
-    def __init__(self, *args, **kwargs):
-        """ Initializing new User class """
-        super().__init__(**{key: kwargs.get(key) for key in User.bkeys
-                            if kwargs.get(key) is not None})
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key not in User.bkeys:
-                    self.__dict__[key] = value
+    """
+    Implementation for User class
+    Public class attributes:
+    email: string - empty string
+    password: string - empty string
+    first_name: string - empty string
+    last_name: string - empty string
+    """
+    self.email = ""
+    self.password = ""
+    self.first_name = ""
+    self.last_name = ""
