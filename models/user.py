b4bc4dba0d294c9277fd5415b0ae98732dcc1ff7
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-10-17 01:45:31 -0700

    updated user

--- models/user.py
+++ models/user.py
@@ -1,18 +1,14 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """ This module defines all attributes and methods of the User class """
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """
-    Implementation for User class
-    Public class attributes:
-    email: string - empty string
-    password: string - empty string
-    first_name: string - empty string
-    last_name: string - empty string
-    """
-    self.email = ""
-    self.password = ""
-    self.first_name = ""
-    self.last_name = ""
+    """Implementation for User class"""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new User."""
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
