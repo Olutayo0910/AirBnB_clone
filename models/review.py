b00fe37403e55c2eef4b4df8d50136210f22018e
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-10-17 20:10:09 -0700

    update classes

--- models/review.py
+++ models/review.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-""" This module defines all attributes and methods of the Review class """
-import models
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """ Implementation for Review class """
-    rkeys = ["place_id", "user_id", "text"]
+    """Represent a review
 
-    def __init__(self, *args, **kwargs):
-        """ Initializing new Review class """
-        super().__init__(**{key: kwargs.get(key) for key in kwargs.keys()
-                            if key not in Review.rkeys
-                            and kwargs.get(key) is not None})
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
+    Attr:
+        place_id (str): The Place id
+        user_id (str): The User id
+        text (str): The content of the review
 
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key in Review.rkeys:
-                    self.__dict__[key] = value
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
--- models/state.py
+++ models/state.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-""" This module defines all attributes and methods of the State class """
-import models
+"""
+Defines the State class
+"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """ Implementation for State class """
-    skeys = ["name"]
+    """Represent a state
 
-    def __init__(self, *args, **kwargs):
-        """ Initializing new State class """
-        super().__init__(**{key: kwargs.get(key) for key in kwargs.keys()
-                            if key not in State.skeys
-                            and kwargs.get(key) is not None})
-        self.name = ""
+    Attributes:
+        name (str): name of the state
 
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key in State.skeys:
-                    self.__dict__[key] = value
+    """
+    name = ""
