f396a8894136ced2c8ab9e241487b8721d17d71a
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-10-17 19:02:37 -0700

    pcs

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -16,18 +16,19 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class TestFileStorage_instantiation(unittest.TestCase):
     """Tests for testing instantiation of FileStorage class."""
     def test_FileStorage_instantiation_with_arg(self):
         with self.assertRaises(TypeError):
             FileStorage(None)
-            
+
     def test_FileStorage_instantiation_no_args(self):
         self.assertEqual(type(FileStorage()), FileStorage)
-    
+
     def testFileStorage_objects_is_private_dict(self):
         self.assertEqual(dict, type(FileStorage._FileStorage__objects))
-        
+
     def test_FileStorage_file_path_is_private_str(self):
         self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
@@ -97,7 +98,7 @@ class TestFileStorage_methods(unittest.TestCase):
     def test_new_with_None(self):
         with self.assertRaises(AttributeError):
             models.storage.new(None)
-            
+
     def test_new_with_args(self):
         with self.assertRaises(TypeError):
             models.storage.new(BaseModel(), 1)
