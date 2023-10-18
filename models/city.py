f18fb3a652d5f9e6aa69b6654708fc2a157a2c9d
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-10-17 19:57:27 -0700

    city update

--- models/city.py
+++ models/city.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-""" This module defines all attributes and methods of the City class """
-import models
+""" This module defines all attributes and methods of the city class """
 from models.base_model import BaseModel
 
 
-class City(BaseModel):
-    """ Implementation for City class """
-    ckeys = ["state_id", "name"]
+class Amenity(BaseModel):
+    """ Implementation for city class
+    Attributes:
+        state_id (str): The state id.
+        name(str): city name
 
-    def __init__(self, *args, **kwargs):
-        """ Initializing new City class """
-        super().__init__(**{key: kwargs.get(key) for key in kwargs.keys()
-                            if key not in City.ckeys
-                            and kwargs.get(key) is not None})
-        self.state_id = ""
-        self.name = ""
-
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key in City.ckeys:
-                    self.__dict__[key] = value
+    """
+    state_id = ""
+    name = ""
