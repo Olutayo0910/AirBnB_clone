44634fd34b6cc8b51e96a7deacaf6f774c69d8b5
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-10-17 19:53:43 -0700

    updated amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,12 @@
 #!/usr/bin/python3
 """ This module defines all attributes and methods of the Amenity class """
-import models
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """ Implementation for Amenity class """
-    akeys = ["name"]
+    """ Implementation for Amenity class
+    Attributes:
+        name(str): amenity name
 
-    def __init__(self, *args, **kwargs):
-        """ Initializing new Amenity class """
-        super().__init__(**{key: kwargs.get(key) for key in kwargs.keys()
-                            if key not in Amenity.akeys
-                            and kwargs.get(key) is not None})
-        self.name = ""
-
-        if len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key in Amenity.akeys:
-                    self.__dict__[key] = value
+    """
+    name = ""
